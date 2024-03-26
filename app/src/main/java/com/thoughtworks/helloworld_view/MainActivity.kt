51e3012b57cc84813e8efff1224ba15e51fa62a5
Laytonss <jiajun.huang@thoughworks.com>
2024-03-26 15:01:34 +0800

    feat: change xml to dynamic generate code style

--- app/src/main/java/com/thoughtworks/helloworld_view/MainActivity.kt
+++ app/src/main/java/com/thoughtworks/helloworld_view/MainActivity.kt
@@ -1,20 +1,41 @@
 package com.thoughtworks.helloworld_view
 
 import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.marginTop
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        addButtons()
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+    }
+
+    private fun addButtons() {
+        val linearLayout = findViewById<LinearLayout>(R.id.linearLayoutForButtons)
+        for(i in 1..20) {
+            val button = Button(this)
+            button.text = "button $i"
+            val layoutParams = LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+            )
+            layoutParams.topMargin = 16
+            layoutParams.width = 650
+            button.layoutParams = layoutParams
+            linearLayout.addView(button)
         }
     }
 }
