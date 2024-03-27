cfd7abc261c96654509d8fb6a990073f1c84fb87
Laytonss <jiajun.huang@thoughworks.com>
2024-03-27 19:04:24 +0800

    refactor: delete no use import

--- app/src/main/java/com/thoughtworks/helloworld_view/LoginActivity.kt
+++ app/src/main/java/com/thoughtworks/helloworld_view/LoginActivity.kt
@@ -2,7 +2,6 @@ package com.thoughtworks.helloworld_view
 
 import android.os.Bundle
 import android.view.View
-import android.view.WindowInsetsController
 import androidx.appcompat.app.AppCompatActivity
 
 
