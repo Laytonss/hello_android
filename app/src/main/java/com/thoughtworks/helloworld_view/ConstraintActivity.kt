c53790a3aefe8b890453945732084b61775be762
Laytonss <jiajun.huang@thoughworks.com>
2024-03-27 16:11:56 +0800

    feat: add style and screen shot

--- app/src/main/java/com/thoughtworks/helloworld_view/ConstraintActivity.kt
+++ app/src/main/java/com/thoughtworks/helloworld_view/ConstraintActivity.kt
@@ -1,14 +1,11 @@
 package com.thoughtworks.helloworld_view
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class ConstraintActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.constraint_layout)
     }
 }
