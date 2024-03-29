ab66ba330a79e622ee3213a966939f1152e7dc19
Laytonss <jiajun.huang@thoughworks.com>
2024-03-29 21:56:16 +0800

    feat: add feature toggle for test

--- app/src/main/java/com/thoughtworks/helloworld_view/TweetsActivity.kt
+++ app/src/main/java/com/thoughtworks/helloworld_view/TweetsActivity.kt
@@ -10,6 +10,8 @@ import com.thoughtworks.helloworld_view.model.Tweet
 
 class TweetsActivity : AppCompatActivity() {
 
+    private val is_use_json_file_feature_toggle = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.tweets_layout)
@@ -25,6 +27,16 @@ class TweetsActivity : AppCompatActivity() {
     }
 
     private fun getTweetListData(): ArrayList<Tweet> {
+        if (is_use_json_file_feature_toggle) {
+            return arrayListOf(
+                Tweet("content1", emptyList(), Sender("name1", "nick1", "avatar1"), emptyList()),
+                Tweet("content2", emptyList(), Sender("name2", "nick1", "avatar1"), emptyList()),
+                Tweet("content3", emptyList(), Sender("name3", "nick1", "avatar1"), emptyList()),
+                Tweet("content4", emptyList(), Sender("name4", "nick1", "avatar1"), emptyList()),
+                Tweet("content5", emptyList(), Sender("name5", "nick1", "avatar1"), emptyList()),
+                Tweet("content6", emptyList(), Sender("name6", "nick1", "avatar1"), emptyList()),
+            )
+        }
         return arrayListOf(
             Tweet("content1", emptyList(), Sender("name1", "nick1", "avatar1"), emptyList()),
             Tweet("content2", emptyList(), Sender("name2", "nick1", "avatar1"), emptyList()),
