bb78c8f8b51b2f2687cfe750d4a5b49ff56f8984
Laytonss <jiajun.huang@thoughworks.com>
2024-03-29 23:18:18 +0800

    feat: add basic implements for bottom message show

--- app/src/main/java/com/thoughtworks/helloworld_view/TweetsActivity.kt
+++ app/src/main/java/com/thoughtworks/helloworld_view/TweetsActivity.kt
@@ -1,6 +1,8 @@
 package com.thoughtworks.helloworld_view
 
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -21,10 +23,29 @@ class TweetsActivity : AppCompatActivity() {
 
     private fun initUI() {
         val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        val bottomTextView = findViewById<TextView>(R.id.bottom_text_id)
         recyclerView.layoutManager = LinearLayoutManager(this)
-        getTweetListData()
         val tweetAdapter = TweetAdapter(getTweetListData())
         recyclerView.adapter = tweetAdapter
+        configBottomHintMessage(recyclerView, bottomTextView)
+    }
+
+    private fun configBottomHintMessage(recyclerView: RecyclerView, bottomTextView: TextView) {
+        recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                super.onScrollStateChanged(recyclerView, newState)
+                if (newState == RecyclerView.SCROLL_STATE_IDLE) {
+                    val layoutManager = recyclerView.layoutManager as LinearLayoutManager
+                    val lastVisibleItemPosition = layoutManager.findLastVisibleItemPosition()
+                    val totalItemCount = recyclerView.adapter!!.itemCount
+                    if (lastVisibleItemPosition == totalItemCount - 1) {
+                        bottomTextView.visibility = TextView.VISIBLE
+                    } else {
+                        bottomTextView.visibility = TextView.INVISIBLE
+                    }
+                }
+            }
+        })
     }
 
     private fun getTweetListData(): List<Tweet> {
