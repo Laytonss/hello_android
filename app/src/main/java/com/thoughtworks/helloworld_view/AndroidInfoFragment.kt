85675eb5f4bf1084b39261d43bdf95a604cbcb04
Laytonss <jiajun.huang@thoughworks.com>
2024-03-29 14:00:06 +0800

    feat: add basic implements for fragment page

---
+++ app/src/main/java/com/thoughtworks/helloworld_view/AndroidInfoFragment.kt
@@ -0,0 +1,13 @@
+package com.thoughtworks.helloworld_view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class AndroidInfoFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_android_info, container, false)
+    }
+}
--- app/src/main/java/com/thoughtworks/helloworld_view/LanguageSelectionActivity.kt
+++ app/src/main/java/com/thoughtworks/helloworld_view/LanguageSelectionActivity.kt
@@ -1,24 +1,31 @@
 package com.thoughtworks.helloworld_view
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class LanguageSelectionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.language_selection_layout)
-        addFragment()
+        modifyNavigation()
     }
 
-    private fun addFragment() {
-        val navigationFragment = NavigationFragment()
-        val transaction = supportFragmentManager.beginTransaction()
-        transaction.add(R.id.fragment_container_navigation, navigationFragment)
-        transaction.commit()
+    private fun modifyNavigation() {
+        val androidButton = findViewById<Button>(R.id.button1)
+        val javaButton = findViewById<Button>(R.id.button2)
+        androidButton.setOnClickListener {
+            val androidInfoFragment = AndroidInfoFragment()
+            val transaction2 = supportFragmentManager.beginTransaction()
+            transaction2.add(R.id.fragment_container_info, androidInfoFragment)
+            transaction2.commit()
+        }
 
-        val infoFragment = InfoFragment()
-        val transaction2 = supportFragmentManager.beginTransaction()
-        transaction2.add(R.id.fragment_container_info, infoFragment)
-        transaction2.commit()
+        javaButton.setOnClickListener {
+            val javaInfoFragment = JavaInfoFragment()
+            val transaction2 = supportFragmentManager.beginTransaction()
+            transaction2.add(R.id.fragment_container_info, javaInfoFragment)
+            transaction2.commit()
+        }
     }
 }
