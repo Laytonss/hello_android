9c22fc49dfa763b2b527d399d5ed98d112ffb993
Laytonss <jiajun.huang@thoughworks.com>
2024-03-29 22:16:17 +0800

    refactor: change position for generate method

--- app/src/test/java/com/thoughtworks/helloworld_view/ExampleUnitTest.kt
+++ app/src/test/java/com/thoughtworks/helloworld_view/ExampleUnitTest.kt
@@ -13,17 +13,5 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
-    @Test
-    fun generateTweetListJson() {
-        val gson = Gson()
-        val tweetListData = arrayListOf(
-            Tweet("content1", emptyList(), Sender("name1", "nick1", "avatar1"), emptyList()),
-            Tweet("content2", emptyList(), Sender("name2", "nick1", "avatar1"), emptyList()),
-            Tweet("content3", emptyList(), Sender("name3", "nick1", "avatar1"), emptyList()),
-            Tweet("content4", emptyList(), Sender("name4", "nick1", "avatar1"), emptyList()),
-            Tweet("content5", emptyList(), Sender("name5", "nick1", "avatar1"), emptyList()),
-            Tweet("content6", emptyList(), Sender("name6", "nick1", "avatar1"), emptyList()),
-        )
-        println(gson.toJson(tweetListData))
-    }
+
 }
