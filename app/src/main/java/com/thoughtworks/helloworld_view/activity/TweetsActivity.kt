e5c2e255d7f34ac1836924bc608c26516ffe86a3
Laytonss <jiajun.huang@thoughworks.com>
2024-04-03 15:00:44 +0800

    feat: finish use assets to get json data

--- app/src/main/java/com/thoughtworks/helloworld_view/activity/TweetsActivity.kt
+++ app/src/main/java/com/thoughtworks/helloworld_view/activity/TweetsActivity.kt
@@ -10,7 +10,8 @@ import com.thoughtworks.helloworld_view.adapters.TweetAdapter
 import com.thoughtworks.helloworld_view.model.Sender
 import com.thoughtworks.helloworld_view.model.Tweet
 
-const val IS_USE_JSON_FILE_FEATURE_TOGGLE = true
+const val IS_USE_JSON_FILE_FEATURE_TOGGLE = false
+const val IS_USE_ASSET_FILE_FEATURE_TOGGLE = true
 
 class TweetsActivity : AppCompatActivity(R.layout.tweets_layout) {
 
@@ -27,19 +28,26 @@ class TweetsActivity : AppCompatActivity(R.layout.tweets_layout) {
     }
 
     private fun getTweetListData(): List<Tweet> {
-        if (!IS_USE_JSON_FILE_FEATURE_TOGGLE) {
-            return arrayListOf(
-                Tweet("content1", emptyList(), Sender("name1", "nick1", "avatar1"), emptyList()),
-                Tweet("content2", emptyList(), Sender("name2", "nick1", "avatar1"), emptyList()),
-                Tweet("content3", emptyList(), Sender("name3", "nick1", "avatar1"), emptyList()),
-                Tweet("content4", emptyList(), Sender("name4", "nick1", "avatar1"), emptyList()),
-                Tweet("content5", emptyList(), Sender("name5", "nick1", "avatar1"), emptyList()),
-                Tweet("content6", emptyList(), Sender("name6", "nick1", "avatar1"), emptyList()),
-            )
-        }
         val gson = Gson()
-        val inputStream = resources.openRawResource(R.raw.tweets)
-        val json = inputStream.bufferedReader().readText()
-        return gson.fromJson(json, Array<Tweet>::class.java).toList().filter { it.error == null && it.unknownError == null }
+        if (IS_USE_JSON_FILE_FEATURE_TOGGLE) {
+            resources.openRawResource(R.raw.tweets).use { inputStream ->
+                val json = inputStream.bufferedReader().readText()
+                return gson.fromJson(json, Array<Tweet>::class.java).toList().filter { it.error == null && it.unknownError == null }
+            }
+        }
+        if (IS_USE_ASSET_FILE_FEATURE_TOGGLE) {
+            assets.open("tweets.json").use { inputStream ->
+                val json = inputStream.bufferedReader().readText()
+                return gson.fromJson(json, Array<Tweet>::class.java).toList().filter { it.error == null && it.unknownError == null }
+            }
+        }
+        return arrayListOf(
+            Tweet("content1", emptyList(), Sender("name1", "nick1", "avatar1"), emptyList()),
+            Tweet("content2", emptyList(), Sender("name2", "nick1", "avatar1"), emptyList()),
+            Tweet("content3", emptyList(), Sender("name3", "nick1", "avatar1"), emptyList()),
+            Tweet("content4", emptyList(), Sender("name4", "nick1", "avatar1"), emptyList()),
+            Tweet("content5", emptyList(), Sender("name5", "nick1", "avatar1"), emptyList()),
+            Tweet("content6", emptyList(), Sender("name6", "nick1", "avatar1"), emptyList()),
+        )
     }
 }
