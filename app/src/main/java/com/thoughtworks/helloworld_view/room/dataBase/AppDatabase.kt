a0536f7f26d18b3e86992b4b8d56a313d167ad20
Laytonss <jiajun.huang@thoughworks.com>
2024-04-06 15:26:27 +0800

    feat: add db config

--- app/src/main/java/com/thoughtworks/helloworld_view/room/dataBase/AppDatabase.kt
+++ app/src/main/java/com/thoughtworks/helloworld_view/room/dataBase/AppDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.RoomDatabase
 import com.thoughtworks.helloworld_view.room.dao.TweetDao
 import com.thoughtworks.helloworld_view.room.entity.Tweet
 
-@Database(entities = [Tweet::class], version = 1)
+@Database(entities = [Tweet::class], version = 1, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun tweetDao(): TweetDao
 }
