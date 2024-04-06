ae8b53274d617e9023c0be131b7a7d88d44a839d
Laytonss <jiajun.huang@thoughworks.com>
2024-04-06 21:11:25 +0800

    feat: add data list to insert db method

--- app/src/main/java/com/thoughtworks/helloworld_view/activity/TweetsActivity.kt
+++ app/src/main/java/com/thoughtworks/helloworld_view/activity/TweetsActivity.kt
@@ -40,7 +40,7 @@ class TweetsActivity : AppCompatActivity(R.layout.tweets_layout) {
         return runBlocking {
             val tweets = tweetDao.getAll()
             val tweetDataList = tweetDao.getTweetDataList()
-            Log.d("room", "### ${tweetDataList.size} ${tweetDataList.first()}")
+            Log.d("room数据库查询结果", "tweetData个数:${tweetDataList.size} tweetData第一个结果${tweetDataList.first()}")
             tweets.filter { it.error == null && it.unknownError == null }
         }
     }
@@ -52,10 +52,20 @@ class TweetsActivity : AppCompatActivity(R.layout.tweets_layout) {
         val imageDao = application.getImageDao()
         val commentDao = application.getCommentDao()
         runBlocking {
-            senderDao.insertALL(arrayListOf(Sender(1, 1, "name")))
             tweetDao.insertALL(arrayListOf(Tweet(1, "content1", "error", "un error")))
+            senderDao.insertALL(arrayListOf(Sender(1, 1, "name")))
             imageDao.insertALL(arrayListOf(Image(1, 1, "url1"), Image(2, 1, "url2")))
             commentDao.insertALL(arrayListOf(Comment(1, 1, "comment content"), Comment(2, 1, "comment content")))
+
+            tweetDao.insertALL(arrayListOf(Tweet(2, "content1", null, null)))
+            senderDao.insertALL(arrayListOf(Sender(2, 2, "name")))
+            imageDao.insertALL(arrayListOf(Image(3, 2, "url1"), Image(4, 1, "url2")))
+            commentDao.insertALL(arrayListOf(Comment(3, 2, "comment content"), Comment(4, 1, "comment content")))
+
+            tweetDao.insertALL(arrayListOf(Tweet(3, "content1", null, null)))
+            senderDao.insertALL(arrayListOf(Sender(3, 3, "name")))
+            imageDao.insertALL(arrayListOf(Image(5, 3, "url1"), Image(6, 1, "url2")))
+            commentDao.insertALL(arrayListOf(Comment(5, 3, "comment content"), Comment(6, 1, "comment content")))
         }
     }
 }
