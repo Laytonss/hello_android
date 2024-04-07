6dbd2f7626d9b0bec0be43e422478da509f749e4
Laytonss <jiajun.huang@thoughworks.com>
2024-04-07 20:36:49 +0800

    refactor: extract method for prepare tweet data

--- app/src/main/java/com/thoughtworks/helloworld_view/activity/TweetsActivity.kt
+++ app/src/main/java/com/thoughtworks/helloworld_view/activity/TweetsActivity.kt
@@ -1,6 +1,7 @@
 package com.thoughtworks.helloworld_view.activity
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -11,6 +12,7 @@ import com.thoughtworks.helloworld_view.application.MyApplication
 import com.thoughtworks.helloworld_view.room.dataSource.TweetDataSource
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
+import java.io.IOException
 
 
 class TweetsActivity : AppCompatActivity(R.layout.tweets_layout) {
@@ -24,15 +26,20 @@ class TweetsActivity : AppCompatActivity(R.layout.tweets_layout) {
         val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
         recyclerView.layoutManager = LinearLayoutManager(this)
         val tweetAdapter = TweetAdapter()
+        prepareTweetData(tweetAdapter)
+        recyclerView.adapter = tweetAdapter
+    }
 
+    private fun prepareTweetData(tweetAdapter: TweetAdapter) {
         val dataSource = TweetDataSource(applicationContext as MyApplication)
-        dataSource.insertDataToDB()
-
+        try {
+            dataSource.getAndInsertDataToDB()
+        } catch (e: IOException) {
+            Toast.makeText(applicationContext, "network request fail", Toast.LENGTH_LONG).show()
+        }
         val tweetDataListFlow = dataSource.fetchTweets()
         tweetDataListFlow.onEach {
             tweetAdapter.setTweetDataList(it)
         }.launchIn(lifecycleScope)
-
-        recyclerView.adapter = tweetAdapter
     }
 }
--- app/src/main/java/com/thoughtworks/helloworld_view/room/dataSource/TweetDataSource.kt
+++ app/src/main/java/com/thoughtworks/helloworld_view/room/dataSource/TweetDataSource.kt
@@ -1,14 +1,16 @@
 package com.thoughtworks.helloworld_view.room.dataSource
 
-import android.util.Log
 import com.google.gson.Gson
 import com.thoughtworks.helloworld_view.R
 import com.thoughtworks.helloworld_view.application.MyApplication
+import com.thoughtworks.helloworld_view.okHttp.OkHttpRequester
 import com.thoughtworks.helloworld_view.room.entity.Tweet
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.launch
+
+const val TWEET_URL = "https://raw.githubusercontent.com/TW-Android-Junior-Training/android_training_practice/main/json/tweets.json"
 
 class TweetDataSource(private val application: MyApplication) {
 
@@ -19,13 +21,20 @@ class TweetDataSource(private val application: MyApplication) {
         return tweetDao.getAll()
     }
 
-    fun insertDataToDB() {
-        val tweetList = getDataFromLocalJsonFile()
+    fun getAndInsertDataToDB() {
+        //val tweetList = getDataFromLocalJsonFile()
         MainScope().launch(Dispatchers.IO) {
+            val tweetList = getDataFromOkHttp()
             tweetDao.insertALL(tweetList)
         }
     }
 
+    //用okHttp从network读数据
+    private suspend fun getDataFromOkHttp(): List<Tweet> {
+        return gson.fromJson(OkHttpRequester().request(TWEET_URL), Array<Tweet>::class.java).toList()
+    }
+
+    //从json文件读tweet数据
     private fun getDataFromLocalJsonFile(): List<Tweet> {
         application.resources.openRawResource(R.raw.tweets).use { inputStream ->
             val json = inputStream.bufferedReader().readText()
