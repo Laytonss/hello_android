5640fa6331838744e07cf47e6cb17745bd0636e5
Laytonss <jiajun.huang@thoughworks.com>
2024-04-09 11:36:41 +0800

    refactor: delete no use import

--- app/src/main/java/com/thoughtworks/helloworld_view/viewModel/TweetsViewModel.kt
+++ app/src/main/java/com/thoughtworks/helloworld_view/viewModel/TweetsViewModel.kt
@@ -1,16 +1,13 @@
 package com.thoughtworks.helloworld_view.viewModel
 
-import android.app.Application
-import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.ViewModel
-import com.thoughtworks.helloworld_view.application.MyApplication
 import com.thoughtworks.helloworld_view.dataSource.TweetDataSource
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class TweetsViewModel @Inject constructor(
-    val tweetDataSource: TweetDataSource
+    tweetDataSource: TweetDataSource
 ) : ViewModel() {
     val tweetsLiveData = tweetDataSource.fetchTweets()
 }
